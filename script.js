d7c5515c0f58fa892234ce2fa3684a1bca6bfe99
C10H12O2 <Eugene.emmanuel2007@gmail.com>
2025-11-25 01:59:30 +0300

    Added logic containing discounts, profits, and loss

---
+++ script.js
@@ -0,0 +1,28 @@
+function calculate() {
+    let originalPrice = Number(document.getElementById("originalPrice").value)
+    let discountPrice = Number(document.getElementById("discountPrice").value)
+    let sellPrice = Number(document.getElementById("sellPrice").value)
+
+    let discountPercent = (discountPrice / 100) * originalPrice
+    document.getElementById("discountResult").innerHTML = "Discount Amount: " + discountPercent
+
+    let finalPrice = originalPrice - discountPercent
+
+    if (sellPrice > finalPrice) {
+        let profit = sellPrice - finalPrice
+        document.getElementById("profitResult").innerHTML = "Profit: " + profit
+        document.getElementById("lossResult").innerHTML = ""
+    }
+
+    else if (sellPrice < finalPrice) {
+        let loss = finalPrice - sellPrice
+        document.getElementById("lossResult").innerHTML = "Loss: " + loss
+        document.getElementById("profitResult").innerHTML = ""
+    }
+
+    else {
+        document.getElementById("profitResult").innerHTML = "No Profit No Loss"
+        document.getElementById("lossResult").innerHTML = ""
+
+    }
+}
